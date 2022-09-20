fc61c43df8fc3f6406ecbc51c40738db424be134
Tianyu W <tianyuelite@gmail.com>
2022-09-20 13:53:57 -0400

    Create HelloWorld.java

---
+++ Unit 01/HelloWorld.java
@@ -0,0 +1,6 @@
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String []args) { // main program
+       System.out.println("Hello World"); // prints Hello World
+    }
+ }
